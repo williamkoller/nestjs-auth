c8ef52bde9694bedb5286d6371ed53527cb48aab
william koller <wkoller25@gmail.com>
2021-02-04 23:28:54 -0300

    feat: add setGlobalPrefix

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.setGlobalPrefix('api');
   await app.listen(process.env.PORT, () =>
     console.log(
       `Application running and listening at this address: http://localhost:${process.env.PORT}`,
