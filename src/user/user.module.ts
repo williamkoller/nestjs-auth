1a4347b27f689ce5ded21e4419d4467af8349bf7
william koller <wkoller25@gmail.com>
2021-02-04 23:41:16 -0300

    feat: set CreateUserRepository in UserModule

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,9 +4,12 @@ import { User } from '@/user/model/user.model';
 import { ListUserRepository } from '@/user/repositories/list-user.repository';
 import { ListUserService } from '@/user/services/list-user.service';
 import { ListUserController } from '@/user/controllers/list-user.controller';
+import { CreateUserRepository } from '@/user/repositories/create-user.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, ListUserRepository])],
+  imports: [
+    TypeOrmModule.forFeature([User, ListUserRepository, CreateUserRepository]),
+  ],
   providers: [ListUserService],
   exports: [ListUserService],
   controllers: [ListUserController],
