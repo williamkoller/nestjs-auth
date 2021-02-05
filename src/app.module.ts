a55142217116919fe34a47673c5e2f9820c59a9e
william koller <wkoller25@gmail.com>
2021-02-04 23:17:08 -0300

    refactor: set forwardRef

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeOrmConfig } from '@/configs/orm.config';
 import { UserModule } from '@/user/user.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(TypeOrmConfig), UserModule],
+  imports: [
+    TypeOrmModule.forRoot(TypeOrmConfig),
+    forwardRef(() => UserModule),
+    ConfigModule.forRoot(),
+  ],
   controllers: [],
   providers: [],
 })
